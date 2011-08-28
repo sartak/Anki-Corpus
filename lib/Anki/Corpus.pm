7eb9cea3bf4725833944e3762b8ef919aa624680
Shawn M Moore <sartak@gmail.com>
2011-08-28 12:22:46 -0400

    First pass at Anki::Corpus

---
+++ lib/Anki/Corpus.pm
@@ -0,0 +1,80 @@
+package Anki::Corpus;
+use Any::Moose;
+use DBI;
+use Params::Validate 'validate';
+
+# ABSTRACT: interact with a corpus of sentences for Anki
+
+has file => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has dbh => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        my $needs_schema = !-e $self->file;
+
+        my $dbh = DBI->connect("dbi:SQLite:dbname=" . $self->file);
+        $dbh->{sqlite_unicode} = 1;
+
+        if ($needs_schema) {
+            $self->schematize($dbh);
+        }
+
+        $dbh
+    },
+    handles => ['prepare'],
+);
+
+sub add_sentence {
+    my $self = shift;
+    my %args = validate(@_, {
+        japanese    => 1,
+        translation => { default => '' },
+        readings    => { default => '' },
+        source      => 1,
+        suspended   => { default => 1, regex => qr/\A[01]\z/ },
+        created     => { default => time },
+        unsuspended => { default => undef },
+    });
+
+    if (!defined($args{unsuspended}) && !$args{suspended}) {
+        $args{unsuspended} = time;
+    }
+
+    my $dbh = $self->dbh;
+    $dbh->do("INSERT INTO sentences (japanese, translation, readings, source, suspended, created, unsuspended) values (?, ?, ?, ?, ?, ?, ?);", {},
+        $args{japanese},
+        $args{translation},
+        $args{readings},
+        $args{source},
+        $args{suspended},
+        $args{created},
+        $args{suspended},
+    );
+}
+
+sub schematize {
+    my $self = shift;
+    my $dbh = shift;
+
+    $dbh->do(<< '    SCHEMA');
+        CREATE TABLE sentences (
+            japanese TEXT UNIQUE NOT NULL,
+            translation TEXT,
+            readings TEXT,
+            source TEXT NOT NULL,
+            suspended BOOLEAN DEFAULT 1,
+            created INTEGER NOT NULL,
+            unsuspended INTEGER NOT NULL,
+            notes TEXT
+        );
+    SCHEMA
+}
+
+1;
+
